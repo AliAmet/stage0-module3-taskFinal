7b0ca54868134bbe4e4b3410d8a0e9ffbc1aaabd
AliAmet <ametov1ali@gmail.com>
2023-10-19 16:17:59 +0600

    First commit

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double celsies = temperatureCelsius;
+        System.out.println(celsies*9/5 + 32);
     }
 }
